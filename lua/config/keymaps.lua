02c7aae18c62a93a6318a0c51a8891bad56fd0a1
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2024-10-05 16:58:18 +0200

    Added some lsp stuff and insert mode movement

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,15 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Toggle nvim tree
-map('n', '<leader>n', ':NvimTreeToggle<CR>', opts)
+map("n", "<leader>n", ":NvimTreeToggle<CR>", opts)
 
 -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
+-- Insert mode movement
+map("i", "<C-h>", "<Left>", opts)
+map("i", "<C-l>", "<Right>", opts)
+map("i", "<C-j>", "<Down>", opts)
+map("i", "<C-k>", "<Up>", opts)
--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,38 +1,48 @@
 return {
-    { 
-        "catppuccin/nvim", 
-        name = "catppuccin", 
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup()
-        end
-    },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    -- Replace with your desired formatters
-                    null_ls.builtins.formatting.prettier, -- For JavaScript, TypeScript, etc.
-                    null_ls.builtins.formatting.stylua,   -- For Lua
-                    null_ls.builtins.formatting.rustfmt,  -- For Rust
-                    -- Add formatters for other languages as needed
-                },
-            })
-        end
-    },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup {
-                plugins = {
-                    spelling = {
-                        enabled = true,
-                    },
-                },
-            }
-        end
-    },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup()
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.prettier, -- For JavaScript, TypeScript, etc.
+					null_ls.builtins.formatting.stylua, -- For Lua
+					null_ls.builtins.formatting.dxfmt, -- For Rust
+					-- null_ls.builtins.diagnostics.eslint, -- For JavaScript
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({
+				plugins = {
+					spelling = {
+						enabled = true,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				check_ts = true,
+			})
+		end,
+	},
 }
