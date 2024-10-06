af79883c1342b60f6449e11db9cf6e05bb24b802
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2024-10-06 13:49:47 +0200

    Made sure nvim uses powershell as terminal

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,12 @@
 local opt = vim.opt
 
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.nofsync = true
+
 -- Line numbers
 opt.number = true
 opt.relativenumber = true
@@ -14,12 +21,22 @@ opt.scrolloff = 10
 
 -- Auto-format on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		vim.lsp.buf.format()
-	end,
+    callback = function()
+        vim.lsp.buf.format()
+    end,
 })
 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Tell nvim to use powershell
+vim.o.shell = "powershell"
+vim.o.shellcmdflag =
+"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
+
+opt.clipboard = "unnamedplus"
