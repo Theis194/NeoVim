7ef07d7f11d0596344e42fbf5b0be6ad151e3fdf
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2024-11-04 23:06:08 +0100

    Changed configuration style of nvim-tree
    Moved nvim-tree view to the right

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,20 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		local config = {}
+
+		-- View settings
+		config.view = {}
+		config.view.width = 40
+		config.view.side = "right"
+		config.view.adaptive_size = true
+
+		config.filters = {}
+		config.filters.dotfiles = true
+
+		config.git = {}
+		config.git.enable = true
+
+		require("nvim-tree").setup(config)
 	end,
 }
