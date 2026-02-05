6ddb1acedd2feb73f9ca4f9ec2dda354b733d0b0
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2026-02-05 21:59:17 +0100

    clangd lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,13 +1,19 @@
 vim.lsp.enable({
     "rust_analyzer",
     "lua_ls",
+<<<<<<< HEAD
     -- "hls",
     -- "eslint",
     -- "ts_ls",
     "denols",
+=======
+    --    "eslint",
+    "ts_ls",
+>>>>>>> 0aaffc6 (clangd lsp)
     "html",
     "tailwindcss",
     "roslyn_ls",
+    "clangd",
 })
 
 vim.diagnostic.config({
