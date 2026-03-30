241a8cd7e5b654e7a9134b10aa687bd5fcc58d6d
Theis Jensen <53541469+Theis194@users.noreply.github.com>
2026-03-30 15:19:50 +0200

    python lsp

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,50 +1,51 @@
 vim.lsp.enable({
-	"rust_analyzer",
-	"lua_ls",
-	"denols",
-	"ts_ls",
-	"html",
-	"tailwindcss",
-	"roslyn_ls",
-	"clangd",
+    "rust_analyzer",
+    "lua_ls",
+    "denols",
+    "ts_ls",
+    "html",
+    "tailwindcss",
+    "roslyn_ls",
+    "clangd",
+    "basedpyright",
 })
 
 vim.diagnostic.config({
-	virtual_lines = false,
-	virtual_text = false,
-	underline = true,
-	update_in_insert = false,
-	severity_sort = true,
-	float = {
-		border = "rounded",
-		source = true, -- Show source of diagnostic
-		header = "", -- Remove the "Diagnostics:" header
-		prefix = function(diagnostic)
-			local icons = {
-				[vim.diagnostic.severity.ERROR] = " ",
-				[vim.diagnostic.severity.WARN] = " ",
-				[vim.diagnostic.severity.INFO] = " ",
-				[vim.diagnostic.severity.HINT] = " ",
-			}
-			return icons[diagnostic.severity]
-		end,
-		format = function(diagnostic)
-			if diagnostic.source then
-				return string.format("%s [%s]", diagnostic.message, diagnostic.source)
-			end
-			return diagnostic.message
-		end,
-	},
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "󰅚 ",
-			[vim.diagnostic.severity.WARN] = "󰀪 ",
-			[vim.diagnostic.severity.INFO] = "󰋽 ",
-			[vim.diagnostic.severity.HINT] = "󰌶 ",
-		},
-		numhl = {
-			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
-			[vim.diagnostic.severity.WARN] = "WarningMsg",
-		},
-	},
+    virtual_lines = false,
+    virtual_text = false,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true, -- Show source of diagnostic
+        header = "", -- Remove the "Diagnostics:" header
+        prefix = function(diagnostic)
+            local icons = {
+                [vim.diagnostic.severity.ERROR] = " ",
+                [vim.diagnostic.severity.WARN] = " ",
+                [vim.diagnostic.severity.INFO] = " ",
+                [vim.diagnostic.severity.HINT] = " ",
+            }
+            return icons[diagnostic.severity]
+        end,
+        format = function(diagnostic)
+            if diagnostic.source then
+                return string.format("%s [%s]", diagnostic.message, diagnostic.source)
+            end
+            return diagnostic.message
+        end,
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚 ",
+            [vim.diagnostic.severity.WARN] = "󰀪 ",
+            [vim.diagnostic.severity.INFO] = "󰋽 ",
+            [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+            [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+    },
 })
